c40b401a69f0f9c696f0fb45cdc017b33eabd076
AmTungAnh <anhnttph23200@fpt.edu.vn>
2023-07-07 00:20:45 +0700

    Update service

---
+++ src/main/java/com/example/website_ban_ao_the_thao_psg/service/impl/CauThuServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.website_ban_ao_the_thao_psg.service.impl;
+
+import com.example.website_ban_ao_the_thao_psg.service.CauThuService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CauThuServiceImpl implements CauThuService {
+}
---
+++ src/main/java/com/example/website_ban_ao_the_thao_psg/service/impl/NhaSanXuatServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.website_ban_ao_the_thao_psg.service.impl;
+
+import com.example.website_ban_ao_the_thao_psg.service.NhaSanXuatService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NhaSanXuatServiceImpl implements NhaSanXuatService {
+}
