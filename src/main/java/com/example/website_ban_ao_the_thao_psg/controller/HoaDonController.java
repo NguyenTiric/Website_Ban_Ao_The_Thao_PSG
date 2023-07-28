eac39b1481c8952afc408b82c1e8c452ff57cc91
TiricNguyen\CongTien <nguyentiric@gmail.com>
2023-07-28 21:47:34 +0700

    update hoa don

--- src/main/java/com/example/website_ban_ao_the_thao_psg/controller/HoaDonController.java
+++ src/main/java/com/example/website_ban_ao_the_thao_psg/controller/HoaDonController.java
@@ -2,7 +2,6 @@ package com.example.website_ban_ao_the_thao_psg.controller;
 
 import com.example.website_ban_ao_the_thao_psg.model.response.HoaDonResponse;
 import com.example.website_ban_ao_the_thao_psg.service.HoaDonService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.ParseException;
-import java.util.Date;
 
 @Controller
 @RequestMapping("/admin/psg/hoa-don")
