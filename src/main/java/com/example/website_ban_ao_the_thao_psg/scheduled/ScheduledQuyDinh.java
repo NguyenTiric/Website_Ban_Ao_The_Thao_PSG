bfa6df899b58b30b617c1c254ed1c24f96fa2316
Huytvph23476 <huytvph23476@fpt.edu.vn>
2023-08-09 00:03:57 +0700

    update voucher

--- src/main/java/com/example/website_ban_ao_the_thao_psg/scheduled/ScheduledQuyDinh.java
+++ src/main/java/com/example/website_ban_ao_the_thao_psg/scheduled/ScheduledQuyDinh.java
@@ -16,45 +16,45 @@ import java.util.List;
 @Component
 public class ScheduledQuyDinh {
 
-    @Autowired
-    private JavaMailSender emailSender;
+//    @Autowired
+//    private JavaMailSender emailSender;
+//
+//    @Autowired
+//    private QuyDinhRepository quyDinhRepository;
+//
+//    @Autowired
+//    private NhanVienRepository nhanVienRepository;
+//
+//    @Scheduled(fixedRate = 6000)
+//    public void thongBaoReset() {
+//        LocalDate currentDate = LocalDate.now();
+//        List<QuyDinh> listQuyDinh = quyDinhRepository.findByNgayCapNhatByTrangThai();
+//
+//        for (QuyDinh quyDinh : listQuyDinh) {
+//            LocalDate ngayDatLaiThuHang = quyDinh.getNgayDatLaiThuHang();
+//            LocalDate ngayThongBao = ngayDatLaiThuHang.minusDays(30);
 
-    @Autowired
-    private QuyDinhRepository quyDinhRepository;
-
-    @Autowired
-    private NhanVienRepository nhanVienRepository;
-
-    @Scheduled(fixedRate = 6000)
-    public void thongBaoReset() {
-        LocalDate currentDate = LocalDate.now();
-        List<QuyDinh> listQuyDinh = quyDinhRepository.findByNgayCapNhatByTrangThai();
-
-        for (QuyDinh quyDinh : listQuyDinh) {
-            LocalDate ngayDatLaiThuHang = quyDinh.getNgayDatLaiThuHang();
-            LocalDate ngayThongBao = ngayDatLaiThuHang.minusDays(30);
-
-            if (ngayDatLaiThuHang.isEqual(currentDate)) {
-                List<KhachHang> khachHangList = nhanVienRepository.findAll();
-                nhanVienRepository.resetSoLuongDonHangThanhCongAndSoTienDaChiTieuVeKhong();
-                for (KhachHang khachHang : khachHangList) {
-                    SimpleMailMessage message = new SimpleMailMessage();
-                    message.setTo(khachHang.getEmail());
-                    message.setSubject("Thông báo đặt lại thứ hạng");
-                    message.setText("Xin chào " + khachHang.getTen() + ",\n\nĐến trước 1 tháng nữa, chúng ta sẽ đặt lại thứ hạng. Vui lòng chuẩn bị cho điều này.\n\nTrân trọng,\nWebsite của chúng tôi");
-                    emailSender.send(message);
-                }
-            } else if (ngayDatLaiThuHang.isEqual(ngayThongBao)) {
-                List<KhachHang> khachHangList = nhanVienRepository.findAll();
-                for (KhachHang khachHang : khachHangList) {
-                    SimpleMailMessage message = new SimpleMailMessage();
-                    message.setTo(khachHang.getEmail());
-                    message.setSubject("Thông báo đặt lại thứ hạng");
-                    message.setText("Xin chào " + khachHang.getTen() + ",\n\nĐến trước 1 tháng nữa, chúng ta sẽ đặt lại thứ hạng. Vui lòng chuẩn bị cho điều này.\n\nTrân trọng,\nWebsite của chúng tôi");
-                    emailSender.send(message);
-                }
-            }
-        }
-    }
+//            if (ngayDatLaiThuHang.isEqual(currentDate)) {
+//                List<KhachHang> khachHangList = nhanVienRepository.findAll();
+//                nhanVienRepository.resetSoLuongDonHangThanhCongAndSoTienDaChiTieuVeKhong();
+//                for (KhachHang khachHang : khachHangList) {
+//                    SimpleMailMessage message = new SimpleMailMessage();
+//                    message.setTo(khachHang.getEmail());
+//                    message.setSubject("Thông báo đặt lại thứ hạng");
+//                    message.setText("Xin chào " + khachHang.getTen() + ",\n\nĐến trước 1 tháng nữa, chúng ta sẽ đặt lại thứ hạng. Vui lòng chuẩn bị cho điều này.\n\nTrân trọng,\nWebsite của chúng tôi");
+//                    emailSender.send(message);
+//                }
+//            } else if (ngayDatLaiThuHang.isEqual(ngayThongBao)) {
+//                List<KhachHang> khachHangList = nhanVienRepository.findAll();
+//                for (KhachHang khachHang : khachHangList) {
+//                    SimpleMailMessage message = new SimpleMailMessage();
+//                    message.setTo(khachHang.getEmail());
+//                    message.setSubject("Thông báo đặt lại thứ hạng");
+//                    message.setText("Xin chào " + khachHang.getTen() + ",\n\nĐến trước 1 tháng nữa, chúng ta sẽ đặt lại thứ hạng. Vui lòng chuẩn bị cho điều này.\n\nTrân trọng,\nWebsite của chúng tôi");
+//                    emailSender.send(message);
+//                }
+//            }
+//        }
+//    }
 
 }
